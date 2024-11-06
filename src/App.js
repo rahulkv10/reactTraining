84fc3c961c0a6156466ed6de1574a0066ed06e89
Rahul KV <rahul.kv@inapp.com>
2024-11-06 17:22:08 +0530

    Add assignment1 with UI fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component1 } from './Component1';
+import { Component2 } from './Component2';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="column">
+        <Component1 />
+      </div>
+      <div className="column">
+        <Component2 />
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Component2.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Component2.css"; 
+
+export function Component2() {
+  let title = "Random Texts";
+
+  const generateRandomText = () => {
+    const length = Math.floor(Math.random() * (64 - 8 + 1)) + 8;
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+  };
+
+  const randomTextList = Array.from({ length: 5 }, () => generateRandomText());
+
+  function getHeading() {
+    return title;
+  }
+
+  return (
+    <div className="component2-container">
+      <h1 className="title">{getHeading()}</h1>
+      <ul className="random-text-list">
+        {randomTextList.map((text, index) => (
+          <li className="random-text-item" key={index}>{text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
