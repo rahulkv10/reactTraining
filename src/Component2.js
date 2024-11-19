c6741dee16284229855f15a553dc57fc7973dd9c
Rahul KV <rahul.kv@inapp.com>
2024-11-19 19:24:37 +0530

    Convert to Class componenet and added life cycle methods

--- src/Component2.js
+++ src/Component2.js
@@ -1,32 +1,70 @@
-import "./Component2.css"; 
+import React, { Component } from "react";
+import "./Component2.css";
 
-export function Component2() {
-  let title = "Random Texts";
+export class Component2 extends Component {
+  constructor(props) {
+    super(props);
 
-  const generateRandomText = () => {
+    this.state = {
+      title: "Random Texts",
+      randomTextList: [], 
+    };
+
+    this.generateRandomText = this.generateRandomText.bind(this);
+  }
+
+  generateRandomText() {
     const length = Math.floor(Math.random() * (64 - 8 + 1)) + 8;
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$$%^&*()_";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$$%^&*()_";
     let result = "";
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result;
-  };
-
-  const randomTextList = Array.from({ length: 5 }, () => generateRandomText());
-
-  function getHeading() {
-    return title;
   }
 
-  return (
-    <div className="component2-container">
-      <h1 className="title">{getHeading()}</h1>
-      <p className="random-text-list">
-        {randomTextList.map((text, index) => (
-          <li className="random-text-item" key={index}>{text}</li>
-        ))}
+  componentDidMount() {
+    console.log("print shshhshshshhs");
+
+    setTimeout(() => {
+      const randomTextList = Array.from({ length: 5 }, () =>
+        this.generateRandomText()
+      );
+      this.setState({ randomTextList });
+    }, 10000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.randomTextList !== this.state.randomTextList) {
+      console.log("component updated.");
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Component is about to unmount.");
+  }
+
+  renderRandomTextList() {
+    return this.state.randomTextList.map((text, index) => (
+      <p className="random-text-item" key={index}>
+        {text}
       </p>
-    </div>
-  );
+    ));
+  }
+
+  render() {
+    return (
+      <div className="component2-container">
+        <h1 className="title">{this.state.title}</h1>
+        <p className="random-text-list">
+          {this.state.randomTextList.length > 0 ? (
+            this.renderRandomTextList()
+          ) : (
+            <span>Loading items...</span>
+          )}
+        </p>
+      </div>
+    );
+  }
 }
