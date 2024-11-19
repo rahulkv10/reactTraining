4cd5ca0c59e20a780b87aa8fc07d9b24051a4c40
Rahul KV <rahul.kv@inapp.com>
2024-11-19 19:24:10 +0530

    Convert to class component and to do list as a json list

--- src/Component1.js
+++ src/Component1.js
@@ -1,35 +1,54 @@
-import React from "react";
-import "./Component1.css"; 
+import React, { Component } from "react";
+import "./Component1.css";
 
-export function Component1() {
-  const title = "To Do Items";
+export class Component1 extends Component {
+  constructor(props) {
+    super(props);
 
-  const sampleData = [
-    "Drink Water",
-    "Read newspaper",
-    "Do the Exercise",
-    "Breakfast",
-    "Read a book",
-  ];
-
-  function getSampleData() {
-    return sampleData.map((data, index) => {
-      return (
-        <div className="item-card" key={index}>
-          {data}
-        </div>
-      );
-    });
+    this.state = {
+      title: "To Do Items",
+      todoItems: [
+        { text: "Drink Water" },
+        { text: "Read Newspaper" },
+        { text: "Do the Exercise" },
+        { text: "Breakfast" },
+        { text: "Read a Book" },
+      ],
+    };
   }
 
-  function getHeading() {
-    return title;
+  componentDidMount() {
+    console.log("Component has mounted.");
   }
 
-  return (
-    <div className="component-container">
-      <h1 className="title">{getHeading()}</h1>
-      <div className="dataSet">{getSampleData()}</div>
-    </div>
-  );
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todoItems !== this.state.todoItems) {
+      console.log("Todo items have been updated.");
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Component is about to unmount.");
+  }
+
+  renderTodoItems() {
+    return this.state.todoItems.map((item, index) => (
+      <div className="item-card" key={index}>
+        {item.text}
+      </div>
+    ));
+  }
+
+  renderTitle() {
+    return this.state.title;
+  }
+
+  render() {
+    return (
+      <div className="component-container">
+        <h1 className="title">{this.renderTitle()}</h1>
+        <div className="dataSet">{this.renderTodoItems()}</div>
+      </div>
+    );
+  }
 }
